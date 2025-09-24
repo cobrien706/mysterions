0c87c365f3753a2b59ee96cd69e7689bb9a4f3f6
revwaldo <cobrien706@gmail.com>
2025-09-24 13:56:39 -0400

    try another sys.path.insert

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
