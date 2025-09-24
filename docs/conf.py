d2df8053d4d293b812479d9eeb881bd2d775bdf7
revwaldo <cobrien706@gmail.com>
2025-09-23 22:43:15 -0400

    conf.py - tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -3,16 +3,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../src'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 # autodoc_mock_imports = ["test","tmc"]
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'mysterions'
-copyright = '2025, Christopher O\'Brien'
+copyright = "2025, Christopher O'Brien"
 author = "Christopher O'Brien"
 release = '1.0.0'
 
@@ -53,12 +53,12 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 # epub_show_urls = 'footnote'
 
-# def skip(app, what, name, obj, would_skip, options):
-#     if name == "__init__":
-#         return False
-#     return would_skip
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
 
-# def setup(app):
-#     app.connect("autodoc-skip-member", skip)
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
 
 # something
