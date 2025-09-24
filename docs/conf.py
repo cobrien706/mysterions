ddfeda74576ebd66c97c2c29f626af4d6ccdc488
revwaldo <cobrien706@gmail.com>
2025-09-24 13:37:33 -0400

    removed sys.path.insert from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ release = '1.0.0'
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
+# sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
