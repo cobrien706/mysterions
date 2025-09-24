1279ee0f74138d13cef3a3e7945474a17f7047b6
revwaldo <cobrien706@gmail.com>
2025-09-24 13:29:11 -0400

    added imports to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+
 sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
