adda4d0a6e2f4113ce01f3c3779c869f03c84a10
revwaldo <cobrien706@gmail.com>
2025-09-24 22:07:51 -0400

    master_doc contents

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+master_doc = 'contents'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
